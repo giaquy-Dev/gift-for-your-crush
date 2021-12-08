18754397ae337bfa8147e7d1b7dc49b649683afc
ngthuy-2k7 <95755436+ngthuy-2k7@users.noreply.github.com>
2021-12-08 20:50:00 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     "Cô chấp nhận lời xinloi của bọn em được không ạ ^^",
   buttonYes: "Cô đồng ý, cô sẽ nhận lời xin lỗi của mấy đứa",
   buttonNo: "Hmm... Thôi để cô suy nghĩ thêm đã.",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Cảm ơn cô vì đã đồng ý",
   descriptionModar:
     'Tụi con biết là cô sẽ đồng ý tha lỗi cho tụi em mà ^^ </br> Cám ơn cô đã đồng ý tha lỗi cho tụi em </br> "Tụi em hứa sẽ tương tác với cô nhiều hơn và không vào lớp muộn nữa </br>  Nếu cô đã xem hết rồi, cô hãy chuyển qua nhóm zalo và nhắn cho chúng em biết đi ạ :33 "',
 };
