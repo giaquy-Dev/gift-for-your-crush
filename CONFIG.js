581b6b14cefdd417258d7e8fda61170268c54293
ngthuy-2k7 <95755436+ngthuy-2k7@users.noreply.github.com>
2021-12-08 21:42:13 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,6 +1,17 @@
 const CONFIGDATA = {
   titleWeb: "Tụi em xin lỗi cô",
   titleHeader: "Cô ơi, cô cho tụi em xin lỗi vì chiều nay đã làm không được vui",
+  title: 'Tụi em xin lỗi vì chiều nay đã làm cô buồn',
+    desc: 'Cô có thể tha lỗi cho tụi em được hông ạ ^^',
+    btnYes: 'Ừ, cô sẽ tha lỗi cho mấy đứa',
+    btnNo: 'Không nha :3',
+    question: 'Tại sao cô lại tha lỗi cho tụi em vậy',
+    btnReply: 'Gửi cho các em',
+    reply: 'Tại cô yêu tụi em nhiều lắm luôn ó :33',
+    mess: 'Hihi, tụi em biết mà, tụi em cũng yêu cô nhiềuuuuu',
+    messDesc: 'Chủ nhật tuần này mình lại học tiếp nha cô, tụi em hứa sẽ tương tác đầy đủ và không vào muộn nữa',
+    btnAccept: 'Okiiiii lun <3',
+    descriptionModar: '  Nếu cô đã xem hết rồi, cô hãy chuyển qua nhóm zalo và nhắn cho chúng em biết đi ạ :33 ',
   descriptionHeader:
     "Cô chấp nhận lời xinloi của bọn em được không ạ ^^",
   buttonYes: "Cô đồng ý, cô sẽ nhận lời xin lỗi của mấy đứa",
